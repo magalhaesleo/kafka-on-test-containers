843be19a89dfe419d5777388b2fe0ffa85294dad
Leonardo Magalhães Andrade <leonardo.mandrade@stone.com.br>
2025-04-12 22:51:15 -0300

    configure schema registry

--- src/KafkaFixture.cs
+++ src/KafkaFixture.cs
@@ -1,9 +1,38 @@
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
 using Testcontainers.Kafka;
 using Testcontainers.Xunit;
 using Xunit.Abstractions;
 
 namespace kafka_test_containers;
 
-public class KafkaFixture(IMessageSink messageSink) : ContainerFixture<KafkaBuilder, KafkaContainer>(messageSink)
+public class KafkaFixture(IMessageSink messageSink) : IAsyncLifetime
 {
+    public KafkaContainer KafkaContainer { get; } = BuildKafka();
+    public IContainer SchemaRegistry { get; } = BuildSchemaRegistry();
+    private static KafkaContainer BuildKafka() => new KafkaBuilder().Build();
+    private static IContainer BuildSchemaRegistry()
+    {
+        return new ContainerBuilder()
+            .WithImage("confluentinc/cp-schema-registry:7.5.2")
+            .WithExposedPort(8081)
+            .WithPortBinding(8081, true)
+            .WithEnvironment("SCHEMA_REGISTRY_HOST_NAME", "schema-registry")
+            .WithEnvironment("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:8081")
+            .WithEnvironment("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS", $"PLAINTEXT://kafka:{KafkaBuilder.KafkaPort}")
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilMessageIsLogged("Server started, listening for requests..."))
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await KafkaContainer.StartAsync();
+        await SchemaRegistry.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await KafkaContainer.DisposeAsync();
+        await SchemaRegistry.DisposeAsync();
+    }
 }
--- src/KafkaTests.cs
+++ src/KafkaTests.cs
@@ -1,20 +1,74 @@
+using Avro;
+using Avro.Generic;
 using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using DotNet.Testcontainers.Builders;
+using Testcontainers.Kafka;
 
 namespace kafka_test_containers;
 
-public class KafkaTests(KafkaFixture kafkaFixture) : IClassFixture<KafkaFixture>
+public class KafkaTests
 {
     [Fact]
     public async Task Test1()
     {
+        // Arrange
+        await using var kafkaNetwork = new NetworkBuilder().Build();
+        await kafkaNetwork.CreateAsync();
+        await using var kafkaContainer = new KafkaBuilder()
+            .WithNetwork(kafkaNetwork)
+            .WithNetworkAliases("kafka")
+            .Build();
+        await using var schemaRegistryContainer = new ContainerBuilder()
+            .DependsOn(kafkaContainer)
+            .WithImage("confluentinc/cp-schema-registry:7.5.2")
+            .WithNetwork(kafkaNetwork)
+            .WithExposedPort(8081)
+            .WithPortBinding(8081, true)
+            .WithEnvironment("SCHEMA_REGISTRY_HOST_NAME", "schema-registry")
+            .WithEnvironment("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:8081")
+            .WithEnvironment("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS",
+                $"PLAINTEXT://kafka:{KafkaBuilder.BrokerPort}")
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilMessageIsLogged("Server started, listening for requests..."))
+            .Build();
+        await Task.WhenAll(kafkaContainer.StartAsync(), schemaRegistryContainer.StartAsync());
         var producerConfig = new ProducerConfig
         {
-            BootstrapServers = kafkaFixture.Container.GetBootstrapAddress(),
+            BootstrapServers = kafkaContainer.GetBootstrapAddress(),
         };
-        
-        using var producer = new ProducerBuilder<Null, string>(producerConfig).Build();
 
-        var produceResult = await producer.ProduceAsync("topic", new Message<Null, string> { Value = Guid.NewGuid().ToString() });
+        var schemaRegistryConfig = new SchemaRegistryConfig
+        {
+            Url = $"http://{schemaRegistryContainer.Hostname}:{schemaRegistryContainer.GetMappedPublicPort(8081)}"
+        };
+
+        using var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
+        using var producer = new ProducerBuilder<string, GenericRecord>(producerConfig)
+            .SetValueSerializer(new AvroSerializer<GenericRecord>(schemaRegistry))
+            .Build();
+
+        // Act
+        const string json = """
+            {
+                    "type": "record",
+                    "name": "User",
+                    "fields": [
+                        { "name": "name", "type": "string" },
+                        { "name": "favorite_number",  "type": "long" },
+                        { "name": "favorite_color", "type": "string" }
+                    ]
+                }
+            """;
+        var s = (RecordSchema)Avro.Schema.Parse(json);
+        const long favoriteNumber = 10;
+        var record = new GenericRecord(s);
+        record.Add("name", "Leonardo");
+        record.Add("favorite_number", favoriteNumber);
+        record.Add("favorite_color", "red");
+        var produceResult = await producer.ProduceAsync("topic", new Message<string, GenericRecord> { Value = record });
+
+        // Assert
         Assert.Equal(PersistenceStatus.Persisted, produceResult.Status);
     }
 }
